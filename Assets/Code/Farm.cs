555473eb3ddd8c8eaa5fd8a9aaf1a26ead23356d
herinque <aleherink@seznam.cz>
2018-04-21 10:24:12 +0200

    Test
    test

--- Assets/Code/Farm.cs
+++ Assets/Code/Farm.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Farm : Building {
 
+    GameObject ResManager;
+
     float timer = 10;       //doba za kterou se hráči přičte jídlo.
 
 	void Start () {
         Placed = false;     //budova není zezačátku postavená.
+        ResManager = GameObject.FindGameObjectWithTag("Managers");
     }
 
 	void Update () {
         GeneratingFood();
-        print("Food " + Food);
-        print("Time " + timer);
+        //print("Food " + Food);
+        //print("Time " + timer);
 	}
 
     void GeneratingFood()
@@ -23,7 +26,8 @@ public class Farm : Building {
             timer -= Time.deltaTime;    //Začne odečítat čas.
             if (timer <= 0)             //Pokud bude timer menší nebo roven 0.
             {
-                Food += 10;             //Vygeneruje 10 jídla.
+                ResManager.GetComponent<ResourcesManager>().FoodAmount += 10;
+                //Food += 10;             //Vygeneruje 10 jídla.
                 timer += 10;            //Opět přičte 10 sec do timer a poté se vše opakuje dokola.
             }
         }
