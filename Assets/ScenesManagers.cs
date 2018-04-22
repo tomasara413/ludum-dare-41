7e409ac068524f03e306add196777ac7c64bd319
M0ula <SmoulaKing@seznam.cz>
2018-04-22 23:26:57 +0200

    UI další změny
    UI

---
+++ Assets/ScenesManagers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManagers : MonoBehaviour {
+
+    public void NewGameScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename, LoadSceneMode.Single);        
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
