3879e02b002969720acd69882c873cda61beed15
tomasara413 <tomas.arazim@email.cz>
2018-04-23 16:41:28 +0200

    Towera a projektil jinak

--- Assets/Code/Projectile.cs
+++ Assets/Code/Projectile.cs
@@ -3,30 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-
-    public Rigidbody Rb;
-
-    public Transform Target;
+    public GameObject Target;
 
     public float Speed = 5f;
     public float RotateSpeed = 200f;
 
-    // Use this for initialization
-    void Start () {
-        Target = GameObject.FindGameObjectWithTag("Enemy").transform;
-        Rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        Vector3 direction = Target.position - Rb.position;
-
-        direction.Normalize();
-
-        Vector3 rotateAmount = Vector3.Cross(direction, transform.up);
-
-        Rb.angularVelocity = -rotateAmount * RotateSpeed;
-
-        Rb.velocity = transform.up * Speed;
+    Vector3 direction;
+    private void Update()
+    {
+        if (Target)
+        {
+            direction = Target.transform.position - transform.position;
+            transform.position += direction.normalized * Speed * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(transform.forward, direction), Time.deltaTime);
+        }
     }
 }
--- Assets/Code/Tower.cs
+++ Assets/Code/Tower.cs
@@ -7,28 +7,25 @@ public class Tower : MonoBehaviour {
     public GameObject Bullet;
     public Vector3 Spawn = new Vector3(10, 5, 0);
 
-    public bool Spotted = false;
-
     void Update()
     {
         Shooting();
-        print(Spotted);
     }
 
+    GameObject currentTarget;
     void OnTriggerStay(Collider collision)
     {
         if (collision.gameObject.tag == "Enemy")
-        {
-            Spotted = true;
-            print("Spotted");
-        }
+            currentTarget = collision.gameObject;
     }
 
     void Shooting()
-    {        
-        if (Spotted && Input.GetButtonDown("Fire1"))
+    {
+        if (currentTarget && Input.GetButtonDown("Fire1"))
         {
-            Instantiate(Bullet, Spawn, Bullet.transform.rotation);
+            Projectile proj = Instantiate(Bullet, Spawn, Bullet.transform.rotation).GetComponent<Projectile>();
+            proj.Target = currentTarget;
+            proj.gameObject.SetActive(true);
         }
     }
 }
